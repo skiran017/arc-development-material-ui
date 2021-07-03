865e517a842c51a967f310448cae23b71f434f56
skiran017 <skiran017@gmail.com>
2021-07-04 02:38:20 +0530

    AppBar Logo
    -App bar logo added
    -favicon added
    -using Tabs
    -active tab and its styling
    -navigation with react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
-import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
 
 import Header from './ui/Header';
 import theme from './ui/Theme';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      Hello
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route exact path="/services" component={() => <div>Services</div>} />
+          <Route
+            exact
+            path="/customsoftware"
+            component={() => <div>Custom Software</div>}
+          />
+          <Route
+            exact
+            path="/mobileapps"
+            component={() => <div>Mobile Apps</div>}
+          />
+          <Route exact path="/websites" component={() => <div>Websites</div>} />
+          <Route
+            exact
+            path="/revolution"
+            component={() => <div>The Revolution</div>}
+          />
+          <Route exact path="/about" component={() => <div>About Us</div>} />
+          <Route
+            exact
+            path="/contact"
+            component={() => <div>Contact us</div>}
+          />
+          <Route exact path="/estimate" component={() => <div>Estimate</div>} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
