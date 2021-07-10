1021379827daa85a1f9671ffbd59403fa0d70f3c
skiran017 <skiran017@gmail.com>
2021-07-11 02:49:11 +0530

    animation- services block
    -custom software development
    -ios/Mobile app development
    -website development

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -2,6 +2,7 @@ import { createMuiTheme } from '@material-ui/core';
 
 const arcBlue = '#0b72b9';
 const arcOrange = '#ffba60';
+const arcGrey = '#868686';
 
 export default createMuiTheme({
   palette: {
@@ -35,6 +36,31 @@ export default createMuiTheme({
       fontSize: '2.5rem',
       color: `${arcBlue}`,
       lineHeight: 1.5,
+    },
+    h3: {
+      fontFamily: 'Pacifico',
+      fontSize: '2.5rem',
+      color: arcBlue,
+    },
+    h4: {
+      fontFamily: 'Raleway',
+      fontSize: '1.75rem',
+      color: arcBlue,
+      fontWeight: 700,
+    },
+    subtitle1: {
+      fontSize: '1.25rem',
+      fontWeight: 300,
+      color: arcGrey,
+    },
+    learnButton: {
+      borderColor: arcBlue,
+      color: arcBlue,
+      borderWidth: 2,
+      textTransform: 'none',
+      borderRadius: 50,
+      fontFamily: 'Roboto',
+      fontWeight: 'bold',
     },
   },
   status: {
